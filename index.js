bc48a0b16fccd7758d071429e1205489f975672c
crazyygirl <mounimounika7981@gmail.com>
2024-12-06 12:32:43 +0530

    Added login function

--- index.js
+++ index.js
@@ -8,8 +8,28 @@ function signup(userName) {
 
    
     registeredUsers.push(userName);
-    return "Signup Successful, Please Login";//qadding comments
+    return "Signup Successful, Please Login";
 }
+
+function login(userName, password) {
+    const registeredUsers = ["john_doe", "jane_doe", "user123", "admin"];
+
+    if (registeredUsers.includes(userName)) {
+        if (password === "Emp@123") {
+            return "Login Successful...";
+        } else {
+            return "Wrong Password...";
+        }
+    }
+
+    return "User Not Found, Please Signup";
+}
+
+
+
 
 console.log(signup("john_doe")); 
 console.log(signup("new_user")); 
+console.log(login("john_doe", "Emp@123"));
+console.log(login("john_doe", "WrongPass")); 
+console.log(login("new_user", "Emp@123")); 
